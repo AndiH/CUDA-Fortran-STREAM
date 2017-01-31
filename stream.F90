b499637a71f8ed94b38f702ba5d34a82c1760de3
Andreas Herten <a.herten@fz-juelich.de>
2017-01-31 15:13:42 +0100

    Add comments

--- stream.F90
+++ stream.F90
@@ -1,3 +1,25 @@
+! ------------------------------------------------------------------------------
+! CUDA Fortran F90 variant of the STREAM benchmark (originally: John D McCalpin)
+! Programmer: Andreas Herten
+! ------------------------------------------------------------------------------
+! This program measures the memory transfer rates in GB/s for the four simple
+! kernels of the STREAM benchmark __on the GPU__ using CUDA Fortran.
+! 
+! The code lives at https://github.com/AndiH/CUDA-Fortran-STREAM
+! See also http://www.streambench.org, especially the associated license
+! ------------------------------------------------------------------------------
+! Notes
+! 	- The dp_kind is selectable, as it is in the project I'm using the code for
+!   - CUDA events are used for timings, since they seem easy to handle
+!   - Timings: Max and Avg values are not printed, yet
+!   - CUDA Errors: Those are not handled yet!
+!   - Verification of Results: Also not done yet!
+!   - Makefile is given with PGFortran, which should be the only compiler
+!     capable to generate CUDA code from Fortran
+!   - I actually know very little Fortran. Feel free to correct something on
+!     Github!
+! ------------------------------------------------------------------------------
+
 module datatypes
 	integer, parameter :: DP=13, dp_kind=selected_real_kind(DP)
 end module datatypes
@@ -6,6 +28,9 @@ module gpu
 	use datatypes
 	contains
 	attributes(global) subroutine init(array, value, length)
+		! INIT
+		! Initialize values on GPU device (also functions as warmup)
+		! >> array_j = value
 		implicit none
 		real(kind=dp_kind), dimension(length), intent(inout) :: array
 		real(kind=dp_kind), intent(in), value :: value
@@ -19,6 +44,9 @@ module gpu
 	end subroutine init
 
 	attributes(global) subroutine copy(lhs, rhs, length)
+		! STREAM COPY
+		! Copy values from one array into another
+		! >> lhs_j = rhs_j
 		implicit none
 		real(kind=dp_kind), dimension(length), intent(out) :: lhs
 		real(kind=dp_kind), dimension(length), intent(in) :: rhs
@@ -31,6 +59,9 @@ module gpu
 	end subroutine copy
 
 	attributes(global) subroutine scale(lhs, rhs, scalar, length)
+		! STREAM SCALE
+		! Scale entry of an array and copy to new array
+		! >> lhs_j = scalar * rhs_j
 		implicit none
 		real(kind=dp_kind), dimension(length), intent(out) :: lhs
 		real(kind=dp_kind), dimension(length), intent(in) :: rhs
@@ -44,6 +75,9 @@ module gpu
 	end subroutine scale
 
 	attributes(global) subroutine add(lhs, rhsl, rhsr, length)
+		! STREAM ADD
+		! Add two arrays, put values into other array
+		! >> lhs_j = rhsl_j + rhsr_j
 		implicit none
 		real(kind=dp_kind), dimension(length), intent(out) :: lhs
 		real(kind=dp_kind), dimension(length), intent(in) :: rhsl, rhsr
@@ -56,6 +90,9 @@ module gpu
 	end subroutine add
 
 	attributes(global) subroutine triad(lhs, rhsl, rhsr, scalar, length)
+		! STREAM TRIAD
+		! Multiply entry of array by scalar, add to other array's entry, put values into third array
+		! >> lhs_j = rhsl_j + scalar * rhsr_j
 		implicit none
 		real(kind=dp_kind), dimension(length), intent(out) :: lhs
 		real(kind=dp_kind), dimension(length), intent(in) :: rhsl, rhsr
@@ -77,26 +114,25 @@ program stream
 	use gpu
 	implicit none
 
-	integer(kind=8), parameter :: N = 250000000
-	real(kind=dp_kind) :: value, scalar
-	real(kind=dp_kind), dimension(:), allocatable :: a, d_a, b, d_b, c, d_c
-	attributes(device) :: d_a, d_b, d_c
-	type(dim3) :: nBlocks, nThreads
-	type(cudaEvent) :: startEvent, stopEvent
-	real, dimension(4) :: time
-	real :: tempTime
-	integer :: gpuStatus
+	integer(kind=8), parameter :: N = 250000000  ! Lengths of data arrays
+	real(kind=dp_kind) :: value, scalar  ! Helper variables
+	real(kind=dp_kind), dimension(:), allocatable :: a, d_a, b, d_b, c, d_c  ! Data arrays
+	attributes(device) :: d_a, d_b, d_c  ! Prefixed with d_ for device
+	type(dim3) :: nBlocks, nThreads  ! Launch this many blocks and threads
+	type(cudaEvent) :: startEvent, stopEvent  ! Using CUDA events for timing
+	real, dimension(4) :: time  ! Array of minimal times for each experiment
+	real :: tempTime  ! Helper value for timings
+	integer :: gpuStatus  ! Holds CUDA error values
 	integer(kind=8), dimension(4) :: bytes
-	integer, parameter :: ntimes = 1
-	integer :: i
-	character(len=30) :: outputformat
+	integer, parameter :: ntimes = 10  ! Repeat measurements this often
+	integer :: i  ! Loop run variables
+	character(len=30) :: outputformat  ! FORMAT string for Fortran output
 
 	bytes = (/	2 * sizeof(value) * N, &
 				2 * sizeof(value) * N, &
 				3 * sizeof(value) * N, &
 				3 * sizeof(value) * N /)
 
-	allocate(a(N))
 	allocate(d_a(N))
 	allocate(d_b(N))
 	allocate(d_c(N))
@@ -155,8 +191,8 @@ program stream
 			time(4) = tempTime
 		end if
 	end do
-	! bytes = 2 * sizeof(value) * N
-	! write(*,*) "Bytes: ", bytes
+
+
 	write(*, "(A15, I3, A32)") "Ran benchmarks ", ntimes, " times. Showing minimum results."
 	write(*,*) "-----------------------------------------------"
 	write(*, "(A10, 4x, A11, 4x, A9)") "Experiment", "Rate / GB/s", "Time / ms"
@@ -165,9 +201,5 @@ program stream
 	write(*, outputformat) "Scale", bytes(2)/(time(2)/1000.)/1024/1024/1024, time(2)
 	write(*, outputformat) "Add", bytes(3)/(time(3)/1000.)/1024/1024/1024, time(3)
 	write(*, outputformat) "Triad", bytes(4)/(time(4)/1000.)/1024/1024/1024, time(4)
-
-	! a = d_a
-
-	! write(*,*) a(1), sizeof(a(1))
 
 end program stream
